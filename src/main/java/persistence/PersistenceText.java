3319cc0218f947f5c43b9b36e4df88661c520022
Your Name <you@example.com>
2018-11-24 11:41:30 -0500

    commit after maven

--- src/main/java/persistence/PersistenceText.java
+++ src/main/java/persistence/PersistenceText.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class PersistenceText implements Persistence {
     @Override
     public void save(TodoList todoList) {
+        //把传进来的TodeoList类传变成String
         StringBuilder builder = new StringBuilder();
         builder.append(todoList.getTitle() + "\n");
 
@@ -18,9 +19,11 @@ public class PersistenceText implements Persistence {
         for (TodoItem item : items) {
             builder.append(item.getText() + "\n");
         }
-
+        //String 转 byte[] 并 由memory -> disc
+        //此步并未创建新的文件夹，只是把路径添加进来
         File file = new File("data/todo.txt");
         try {
+            //把builder里的东西存到file里
             FileUtils.writeStringToFile(file,builder.toString(),"UTF-8");
         } catch (IOException e) {
             e.printStackTrace();
