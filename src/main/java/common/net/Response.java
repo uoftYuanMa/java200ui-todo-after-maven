797e04972bdd693fd3a3c049b31d17ac3374737a
Your Name <you@example.com>
2018-12-08 13:50:43 -0500

    构建response

--- src/main/java/common/net/Response.java
+++ src/main/java/common/net/Response.java
@@ -1,4 +1,28 @@
 package common.net;
 
 public class Response {
+    private String status;
+    private String data;
+
+    public static final String statusOk = "ok";
+
+    public Response(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
